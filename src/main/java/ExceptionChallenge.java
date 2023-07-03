cd94d9ef1cafd0cb9ef677a93775b8f89ddee5fe
onusra <onusra@angelo.edu>
2023-07-03 20:21:06 +0000

    Completed

--- src/main/java/ExceptionChallenge.java
+++ src/main/java/ExceptionChallenge.java
@@ -25,6 +25,8 @@ public class ExceptionChallenge {
     public static void main(String [] args){
         
     }
+
+    
     public void mustThrow(){
         throw new ArithmeticException();
     }
